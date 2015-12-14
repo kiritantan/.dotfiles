7a9af37751b01fbc0da5ba60d8ec40ab3ae514b2
kiri <kiri@beru-2.local>
2015-12-14 11:10:40 +0900

    change source of Symbolic link   - .gitignore

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,5 +3,5 @@
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.gitignore ~/.gitignore
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore
 
