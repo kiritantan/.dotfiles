cdc7f25fd9ee145e409a3604cc4afc2f897d37fa
kiri <kiri@beru-2.local>
2016-03-22 04:09:14 +0900

    change path of symbolic link

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/zsh
 
-ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.gitignore_global ~/.gitignore
+ln -s ~/.dotfiles/.vim ~/.vim
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.gitignore_global ~/.gitignore
 
